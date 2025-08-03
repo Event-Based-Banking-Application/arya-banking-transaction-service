b87590dc8a95bab6bc346db601bb9a1dcf44facd
Karthik Kulkarni <64131481+kartik1502@users.noreply.github.com>
2025-08-03 11:49:05 +0530

    Initial commit

---
+++ src/test/java/org/arya/banking/AryaBankingTemplateApplicationTests.java
@@ -0,0 +1,11 @@
+package org.arya.banking;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AryaBankingTemplateApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
